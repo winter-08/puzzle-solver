061af7da0140a1d7dced53d1da129eabc173433c
Winter <Winter@Jonathans-MacBook-Pro.local>
2024-01-05 00:25:39 +0000

    lines

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from tkinter import Tk, BOTH, Canvas
 
 def main():
     win = Window(800, 600)
+    p1 = Point(10, 100)
+    p2 = Point(400, 600)
+    new_line = Line(p1, p2)
+    win.draw_line(new_line, "Red")
     win.wait_for_close()
 
 class Window:
@@ -14,6 +18,8 @@ class Window:
         self.canvas.pack()
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -28,6 +34,20 @@ class Window:
         self.running = False
         self.__root = Tk()
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+        canvas.pack()
 
 if __name__ == "__main__":
     main()
